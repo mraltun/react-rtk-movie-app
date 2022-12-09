b842e5ef0a0cb8bf3728ee95e0ca46a83fba0312
Murat Altun <mraltun@gmail.com>
2022-12-09 16:33:49 +0300

    Header component and it's css done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import user from "../../assets/user.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='header'>
+      <Link to='/'>
+        <div className='logo'>React RTK Movie App</div>
+      </Link>
+      <div className='user-image'>
+        <img src={user} alt='user' />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
