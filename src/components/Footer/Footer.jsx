2af9c390fd42836655e78f7343b53573081e055b
Murat Altun <mraltun@gmail.com>
2022-12-09 17:28:46 +0300

    Added Fotter and it's scss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer'>
+      <div>React RTK Movie App</div>
+      <div>Movie App Inc. ©2022</div>
+    </div>
+  );
 };
 
 export default Footer;
