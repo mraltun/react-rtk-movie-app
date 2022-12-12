4e84a9f820d758df340cba63e1f6f22ffff6e151
Murat Altun <mraltun@gmail.com>
2022-12-12 17:17:17 +0300

    MovieDetails and it's scss is done

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -5,6 +5,7 @@ import {
   fetchAsyncMovieOrShowDetail,
   getSelectedMovieOrShow,
 } from "../../features/movies/movieSlice";
+import "./MovieDetail.scss";
 
 const MovieDetail = () => {
   const { imdbID } = useParams();
@@ -15,7 +16,53 @@ const MovieDetail = () => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
   }, [dispatch, imdbID]);
 
-  return <div>MovieDetail</div>;
+  return (
+    <div className='movie-section'>
+      <div className='section-left'>
+        <div className='movie-title'>{data.Title}</div>
+        <div className='movie-rating'>
+          <span>
+            IMDB Rating <i className='fa fa-star'></i>: {data.imdbRating}
+          </span>
+          <span>
+            IMDB Votes <i className='fa fa-thumbs-up'></i>: {data.imdbVotes}
+          </span>
+          <span>
+            Runtime <i className='fa fa-film'></i>: {data.Runtime}
+          </span>
+          <span>
+            Year <i className='fa fa-calendar'></i>: {data.year}
+          </span>
+        </div>
+        <div className='movie-plot'>{data.Plot}</div>
+        <div className='movie-info'>
+          <div>
+            <span>Director</span>
+            <span>{data.Director}</span>
+          </div>
+          <div>
+            <span>Stars</span>
+            <span>{data.Actors}</span>
+          </div>
+          <div>
+            <span>Genre</span>
+            <span>{data.Genre}</span>
+          </div>
+          <div>
+            <span>Language</span>
+            <span>{data.Languages}</span>
+          </div>
+          <div>
+            <span>Awards</span>
+            <span>{data.Awards}</span>
+          </div>
+        </div>
+      </div>
+      <div className='section-right'>
+        <img src={data.Poster} alt={data.Title} />
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetail;
