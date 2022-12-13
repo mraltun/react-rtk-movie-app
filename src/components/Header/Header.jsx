ae51612ae0c9f9c8c699a9cd2bd25abf6529ea24
Murat Altun <mraltun@gmail.com>
2022-12-13 13:29:24 +0300

    Added search input in Header and it's scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 import user from "../../assets/user.png";
 
 const Header = () => {
+  const [term, setTerm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='header'>
-      <Link to='/'>
-        <div className='logo'>React RTK Movie App</div>
-      </Link>
+      <div className='logo'>
+        <Link to='/'>React RTK Movie App</Link>
+      </div>
+      <div className='search-bar'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={term}
+            placeholder='Search movies or shows'
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <button type='submit'>
+            <i className='fa fa-search'></i>{" "}
+          </button>
+        </form>
+      </div>
       <div className='user-image'>
         <img src={user} alt='user' />
       </div>
